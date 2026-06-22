2c47379f941a8a8a6a39ca527e01d8dcca389f97
Priyanshu Walia <waliapriyanshu07@gmail.com>
2026-06-23 01:59:50 +0530

    fix

---
+++ api/index.ts
@@ -0,0 +1,16 @@
+import app from "../server";
+
+export default function handler(req: any, res: any) {
+  const pathParam = req.query?.path;
+  const path = Array.isArray(pathParam) ? pathParam.join("/") : pathParam;
+
+  if (typeof path === "string" && path.length > 0) {
+    const url = new URL(req.url || "/", "http://localhost");
+    url.searchParams.delete("path");
+
+    const query = url.searchParams.toString();
+    req.url = `/api/${path}${query ? `?${query}` : ""}`;
+  }
+
+  return app(req, res);
+}
